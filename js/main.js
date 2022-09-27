8513d03e6a313efa0dbc87d0cb3704acf57aa6c1
codeeury <pessoahesse@gmail.com>
2022-09-27 22:32:09 +0900

    Start project

---
+++ js/main.js
@@ -0,0 +1,50 @@
+const searchEl = document.querySelector('.search');
+const searchInputEl = document.querySelector('input');
+
+searchEl.addEventListener('click', function () {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function () {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색')
+
+});
+
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '')
+
+});
+
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function() {
+  console.log ('window.scrollY!');
+  if (window.scrollY > 500) {
+    // 배지 숨기기
+    // gsap.to (요소, 지속시간, 옵션)
+    gsap.to(badgeEl, .6, {
+      opacity : 0,
+      display : 'none'
+    });
+  } else {
+    // 배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity : 1,
+      display : 'block'
+    });
+  }
+}, 300)); 
+// _.throttle(함수,시간)
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  // gsap.to(요소, 지속시간, 옵션);
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7, // 0.7, 1.4, 2.1, 2.7
+    opacity : 1
+  });
+});
